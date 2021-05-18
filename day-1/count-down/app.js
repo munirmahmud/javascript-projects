f7b8ffc985d76871d69ad345b30f9868ed1224da
Munir Mahmud <munirmahmud6@gmail.com>
2021-05-18 06:21:34 +0600

    Project done

---
+++ day-1/count-down/app.js
@@ -0,0 +1,26 @@
+const daysEl = document.querySelector("#days");
+const hoursEl = document.querySelector("#hours");
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+
+const newYear = "1 Jan 2022";
+
+const countDown = () => {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
+};
+
+countDown();
+setInterval(countDown, 1000);
