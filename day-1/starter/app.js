9beeef49fdbe3658606157de79ddc1fa68c63b30
Munir Mahmud <munirmahmud6@gmail.com>
2021-05-18 06:19:03 +0600

    Added necessary variables

---
+++ day-1/starter/app.js
@@ -0,0 +1,6 @@
+const daysEl = document.querySelector("#days");
+const hoursEl = document.querySelector("#hours");
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+
+const newYear = "1 Jan 2022";
