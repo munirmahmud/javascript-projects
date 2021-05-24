9bef43ce4c0493ae3eab0c1ac513fa557b4574e9
Munir Mahmud <munirmahmud6@gmail.com>
2021-05-24 08:11:47 +0600

    Js added to observe the button

---
+++ day-7/scroll-observer/app.js
@@ -0,0 +1,19 @@
+const terms = document.querySelector(".terms-and-conditions");
+const watch = document.querySelector(".watch");
+const button = document.querySelector(".accept");
+
+const obCallback = (payload) => {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+};
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
